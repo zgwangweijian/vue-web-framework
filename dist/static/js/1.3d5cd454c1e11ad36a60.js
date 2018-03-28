c9507fe18e45375a87afea468a70fb8d2f00adfe
wangwj <zgwangweijian@163.com>
2018-03-28 20:12:42 +0800

    first version

---
+++ dist/static/js/1.3d5cd454c1e11ad36a60.js
@@ -0,0 +1,2 @@
+webpackJsonp([1],{zKIK:function(e,t,n){"use strict";Object.defineProperty(t,"__esModule",{value:!0});var r={render:function(){var e=this.$createElement;return(this._self._c||e)("p",[this._v(this._s(this.$t("home")))])},staticRenderFns:[]},s=n("VU/8")({name:"home",data:function(){return{}}},r,!1,null,null,null);t.default=s.exports}});
+//# sourceMappingURL=1.3d5cd454c1e11ad36a60.js.map
---
+++ dist/static/js/2.5d66cd627bd0e22787ce.js
@@ -0,0 +1,2 @@
+webpackJsonp([2],{cuRQ:function(e,t,n){"use strict";Object.defineProperty(t,"__esModule",{value:!0});var r={render:function(){this.$createElement;this._self._c;return this._m(0)},staticRenderFns:[function(){var e=this.$createElement,t=this._self._c||e;return t("div",[t("h1",[this._v("page2")])])}]},s=n("VU/8")({name:"page2"},r,!1,null,null,null);t.default=s.exports}});
+//# sourceMappingURL=2.5d66cd627bd0e22787ce.js.map
