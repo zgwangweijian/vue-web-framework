96814b9f51eb6a60be5cf4062857f6b416ba1d9f
wangwj <zgwangweijian@163.com>
2018-04-02 10:27:55 +0800

    delete part unuse code

--- src/store/modules/app.js
+++ src/store/modules/app.js
@@ -1,6 +1,5 @@
 import { otherRouter, appRouter } from '@/router/router'
 import Util from '@/libs/util'
-import Cookies from 'js-cookie'
 import Vue from 'vue'
 
 const app = {
@@ -47,54 +46,7 @@ const app = {
       state.tagsList.push(...list)
     },
     updateMenulist (state) {
-      let accessCode = parseInt(Cookies.get('access'))
-      let menuList = []
-      appRouter.forEach((item, index) => {
-        if (item.access !== undefined) {
-          if (Util.showThisRoute(item.access, accessCode)) {
-            if (item.children.length === 1) {
-              menuList.push(item)
-            } else {
-              let len = menuList.push(item)
-              let childrenArr = []
-              childrenArr = item.children.filter(child => {
-                if (child.access !== undefined) {
-                  if (child.access === accessCode) {
-                    return child
-                  }
-                } else {
-                  return child
-                }
-              })
-              menuList[len - 1].children = childrenArr
-            }
-          }
-        } else {
-          if (item.children.length === 1) {
-            menuList.push(item)
-          } else {
-            let len = menuList.push(item)
-            let childrenArr = []
-            childrenArr = item.children.filter(child => {
-              if (child.access !== undefined) {
-                if (Util.showThisRoute(child.access, accessCode)) {
-                  return child
-                }
-              } else {
-                return child
-              }
-            })
-            if (childrenArr === undefined || childrenArr.length === 0) {
-              menuList.splice(len - 1, 1)
-            } else {
-              let handledItem = JSON.parse(JSON.stringify(menuList[len - 1]))
-              handledItem.children = childrenArr
-              menuList.splice(len - 1, 1, handledItem)
-            }
-          }
-        }
-      })
-      state.menuList = menuList
+      state.menuList = appRouter
     },
     addOpenSubmenu (state, name) {
       let hasThisName = false
