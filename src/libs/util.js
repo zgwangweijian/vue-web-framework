d7c3102ecb65669bdd3b30e7dea4f18a7d423f51
wangwj <zgwangweijian@163.com>
2018-04-02 10:14:23 +0800

    delete part unuse code

--- src/libs/util.js
+++ src/libs/util.js
@@ -1,6 +1,4 @@
 import axios from 'axios'
-import semver from 'semver'
-import packjson from '../../package.json'
 
 let util = {}
 util.title = function (title) {
@@ -9,7 +7,7 @@ util.title = function (title) {
 }
 
 const ajaxUrl = process.env.NODE_ENV === 'development'
-  ? 'http://127.0.0.1:8888'
+  ? ''
   : process.env.NODE_ENV === 'production'
     ? ''
     : ''
@@ -18,32 +16,6 @@ util.ajax = axios.create({
   baseURL: ajaxUrl,
   timeout: 30000
 })
-
-util.inOf = function (arr, targetArr) {
-  let res = true
-  arr.forEach(item => {
-    if (targetArr.indexOf(item) < 0) {
-      res = false
-    }
-  })
-  return res
-}
-
-util.oneOf = function (ele, targetArr) {
-  if (targetArr.indexOf(ele) >= 0) {
-    return true
-  } else {
-    return false
-  }
-}
-
-util.showThisRoute = function (itAccess, currentAccess) {
-  if (typeof itAccess === 'object' && Array.isArray(itAccess)) {
-    return util.oneOf(currentAccess, itAccess)
-  } else {
-    return itAccess === currentAccess
-  }
-}
 
 util.getRouterObjByName = function (routers, name) {
   if (!name || !routers || !routers.length) {
@@ -239,28 +211,6 @@ util.toDefaultPage = function (routers, name, route, next) {
   if (notHandle) {
     next()
   }
-}
-
-util.fullscreenEvent = function (vm) {
-  vm.$store.commit('initCachepage')
-  // 权限菜单过滤相关
-  vm.$store.commit('updateMenulist')
-  // 全屏相关
-}
-
-util.checkUpdate = function (vm) {
-  axios.get('https://api.github.com/repos/iview/iview-admin/releases/latest').then(res => {
-    let version = res.data.tag_name
-    vm.$Notice.config({
-      duration: 0
-    })
-    if (semver.lt(packjson.version, version)) {
-      vm.$Notice.info({
-        title: 'iview-admin更新啦',
-        desc: '<p>iView-admin更新到了' + version + '了，去看看有哪些变化吧</p><a style="font-size:13px;" href="https://github.com/iview/iview-admin/releases" target="_blank">前往github查看</a>'
-      })
-    }
-  })
 }
 
 export default util
